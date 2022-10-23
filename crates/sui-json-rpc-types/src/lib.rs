dc749591a4b2031ec773b9822284865a4906ed6c
patrick <patrickkuo@me.com>
2022-10-23 18:01:09 +0100

    add mutate object explorer display

--- crates/sui-json-rpc-types/src/lib.rs
+++ crates/sui-json-rpc-types/src/lib.rs
@@ -2020,6 +2020,7 @@ pub enum SuiEvent {
         version: SequenceNumber,
     },
     /// Object mutated.
+    #[serde(rename_all = "camelCase")]
     MutateObject {
         package_id: ObjectID,
         transaction_module: String,
