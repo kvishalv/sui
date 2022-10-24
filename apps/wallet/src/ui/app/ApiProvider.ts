594d3fe29fe2a6c4f2951edd759b036bb160e4a9
Chris Li <666lcz@gmail.com>
2022-10-24 14:55:29 -0700

    address comments to add ttl

--- apps/wallet/src/ui/app/ApiProvider.ts
+++ apps/wallet/src/ui/app/ApiProvider.ts
@@ -78,13 +78,9 @@ export default class ApiProvider {
     constructor(private _featureGating: FeatureGating) {}
 
     public setNewJsonRpcProvider(apiEnv: API_ENV = DEFAULT_API_ENV) {
-        this._apiProvider = new JsonRpcProvider(
-            getDefaultAPI(apiEnv).gateway,
-            true
-        );
+        this._apiProvider = new JsonRpcProvider(getDefaultAPI(apiEnv).gateway);
         this._apiFullNodeProvider = new JsonRpcProvider(
-            getDefaultAPI(apiEnv).fullNode,
-            true
+            getDefaultAPI(apiEnv).fullNode
         );
         this._signer = null;
     }
